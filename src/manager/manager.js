f514003150f48e85df17f6ac5f1a65d46e112529
Jorge Falcón <jorgef_campos@outlook.com>
2023-03-19 22:15:40 -0600

    creado EP para obtener productos, creado también el manager de archivos

---
+++ src/manager/manager.js
@@ -0,0 +1,31 @@
+// import fs from "fs";
+const fs = require("fs");
+
+class Manager {
+    constructor(path) {
+        this.path = path;
+    }
+
+    getRecords = async () => {
+        try {
+            if (fs.existsSync(this.path) && (fs.readFileSync(this.path).length !== 0)) { 
+                const products = await fs.promises.readFile(this.path, 'utf-8');
+                console.log(products);
+                return JSON.parse(products);
+            }
+            else {
+                console.info("Creando...");
+                return []
+            }
+        } catch (error) {
+            if (error.message.includes('no such file or directory')) {
+                console.error("\nAl parecer el archivo o directorio no existe.");
+                return [];
+            }
+        }
+    }
+}
+
+module.exports = {
+    Manager
+}
--- src/routes/products.router.js
+++ src/routes/products.router.js
@@ -1,9 +1,50 @@
+const path = require("path");
 const express = require("express");
 const productsRouter = express.Router();
 
+const { Manager } = require("../manager/manager.js")
 
-productsRouter.get("/", (req, res) => {
-    console.log("Hola");
+const dirPath = path.join(__dirname, "../manager/files/products.json");
+const products = new Manager(dirPath);
+
+
+/* middleware que valida lo siguiente:
+    - que exista el queryParam limit
+    - que NO exista un queryParam que NO sea limit
+    - que el queryParam limit tenga un valor
+    - que el queryParam limit tenga un valor numérico válido
+*/
+const validateFormat = (req, res, next) => {
+    const limit = req.query.limit;
+
+    if (limit) {
+        if (limit % 1 !== 0) 
+            return res.status(400).send({"msg": "El parámetro 'limit' no es un número entero valido"});
+        else next()
+    }
+    else {
+        // se valida que la CLAVE limit tenga un VALOR
+        if (limit === "") 
+            return res.status(400).send({"msg": "El parámetro está vacío"});  
+    
+        // existe un queryParam, pero este no es limit
+        if (Object.keys(req.query).length > 0) 
+            res.status(400).send({"msg": `El parámetro requerido 'limit' no se encuentra presente`});
+        else next()
+    }
+}
+
+productsRouter.get("/", validateFormat, async (req, res) => {
+    const limit = req.query.limit;
+
+    await products.getRecords("products").then((resp) => {
+        console.log(`resp: ${resp}`);
+        limit 
+            ? res.json(resp.slice(0, limit))
+            : res.json(resp);
+    }).catch((err) => console.log(`Error: \n${err}`))
+
+
 })
 
 module.exports = {
