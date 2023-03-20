4ad9f5eebfe03f95bcc98625c83ff0a6a011e511
Jorge Falcón <jorgef_campos@outlook.com>
2023-03-19 20:40:28 -0600

    agregada estructura básica de rutas

--- src/app.js
+++ src/app.js
@@ -1,1 +1,16 @@
-console.log("Hello world");
+const express = require("express");
+
+const app = express();
+const PORT = 8080;
+
+
+app.use((err, req, res, next) => {
+    console.log(err);
+    res.status(500).send("Ocurrió un error en el servidor.");
+})
+
+
+app.listen(PORT, (err) => {
+    if (err) return console.log("Error al iniciar el servidor.");
+    console.log(`Servidor corriendo en el puerto ${PORT}`);
+})
